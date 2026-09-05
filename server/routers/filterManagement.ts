b1f53cdc133914e72d259875bbc90925b9bad05c
apdelfatah1975-prog <apdelfatah1975@gmail.com>
2026-09-05 15:06:00 +0300

    Update filterManagement.ts

--- server/routers/filterManagement.ts
+++ server/routers/filterManagement.ts
@@ -1,1 +1,11 @@
+import { router, protectedProcedure } from "../_core/trpc";
+import { z } from "zod";
+import { db } from "../../drizzle/db";
+import { filters } from "../../drizzle/schema";
 
+export const filterManagementRouter = router({
+  list: protectedProcedure.query(async () => {
+    const allFilters = await db.select().from(filters);
+    return allFilters;
+  }),
+});
